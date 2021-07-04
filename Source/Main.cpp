1883c14edc4d485b266df76a158a7dc620c230b0
treenest <ttd8591@gmail.com>
2021-07-05 02:48:56 +0900

    Setting the Initial Hash Value

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,6 +1,10 @@
+// https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.180-4.pdf
+
 #include <iostream>
 #include <string>
 #include <bitset>
+#include <cmath>
+#include <array>
 
 int main() {
 	// input data
@@ -26,7 +30,19 @@ int main() {
 
 	std::bitset<64> bitLength(inputLength);
 	preprocess.append(bitLength.to_string());
-	std::cout << preprocess;
 
+	std::cout << "preprocessed: " << preprocess << std::endl;
+	std::cout << std::endl;
 
+	// Setting the Initial Hash Value
+	int primeList[] = { 2, 3, 5, 7, 11, 13, 17, 19 };
+	int H[8];
+	for (int i = 0; i < 8; ++i)
+	{
+		double x = std::sqrt(primeList[i]);
+		x -= static_cast<uint32_t>(x);
+		x *= std::pow(16, 8);
+		H[i] = static_cast<uint32_t>(x);
+		std::cout << "H" << i << ": " << std::hex <<  H[i] << std::endl;
+	}
 }
